b73de95b7878eca448d67bd11b5b306a22a5fa3e
Santiago Rivera <sarivera.es@tecnologicoargos.edu.ec>
2025-07-21 18:29:24 -0500

    Adds basic extra classes
    Adds two new classes, one to act as an entry point to call the second class.

---
+++ basic/extras/ejerciciosextras.java
@@ -0,0 +1,12 @@
+package basic.extras;
+
+import static basic.extras.extras.exampleMethod;
+
+public class ejerciciosextras {
+    public static void main(String[] args) {
+        // Example usage of the ejerciciosextras class
+        System.out.println("Welcome to the ejerciciosextras class!");
+        // You can add more functionality here as needed
+        exampleMethod();
+    }
+}
---
+++ basic/extras/extras.java
@@ -0,0 +1,18 @@
+package basic.extras;
+
+public class extras {
+
+    public static void main(String[] args) {
+        // Example usage of the extras package
+        System.out.println("Welcome to the extras package!");
+        exampleMethod();
+
+        // You can add more functionality here as needed
+    }
+
+    // Additional methods and classes can be added here
+
+    public static void exampleMethod() {
+        System.out.println("This is an example method in the extras package.");
+    }
+}
